ed01845b11667232b112bf4762ab8c71de1a7812
Pedro Almeida Rocha <48830542+pedrorocha014@users.noreply.github.com>
2020-09-01 15:51:24 -0300

    Update
    - Animais urgentes tem prioridade de atendimento para a especialidade solicitada

--- src/modules/line/filterModule.js
+++ src/modules/line/filterModule.js
@@ -3,8 +3,14 @@ const dataAccess = require('../../modules/dataAccessModule');
 const filterRegister = function(doctorId){
     const doctorData = dataAccess.getDataFromFile("doctorDb.json").find(doctor => doctor.id == doctorId);
     const lineData = dataAccess.getDataFromFile("lineDb.json");
-    const nextTreatment = lineData.find(element => element.specialty == doctorData.specialty);
 
+    //Verifico primeiro se existe emergência
+    var nextTreatment = lineData.find(element => element.specialty == doctorData.specialty && element.urgency == true);
+    
+    if (nextTreatment == null){
+        nextTreatment = lineData.find(element => element.specialty == doctorData.specialty);
+    }
+    
     return nextTreatment;
 }
 
